a5dbc6b50b71431c1db2e4790893903c9d8464c9
Ryan <rbell@email.neit.edu>
2021-04-12 17:22:03 -0400

    bouncing

--- Level 1/js/Ball.js
+++ Level 1/js/Ball.js
@@ -3,14 +3,27 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height/2;
 var vx = 2;
+var radius = 50
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-	ctx.arc(x, y, 50, 0, Math.PI*2);
+	ctx.arc(x, y, radius, 0, Math.PI*2);
 	ctx.fillStyle = "#5A00FF";
 	ctx.fill();
 	ctx.closePath();
 	x += vx;
+
+	if (x + vx > canvas.width-radius)
+	{
+		vx = -vx
+	}
+
+	if (x + vx < 0+radius)
+	{
+		vx = -vx
+	}
 }
+
+
 setInterval(animate, 10);
