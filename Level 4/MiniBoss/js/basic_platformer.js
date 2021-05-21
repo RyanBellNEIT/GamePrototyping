94286b93efb0486bbc135a53b87eec0ef0eb8ac0
Ryan <rbell@email.neit.edu>
2021-05-21 14:09:10 -0400

    Level 4 Mini Boss

---
+++ Level 4/MiniBoss/js/basic_platformer.js
@@ -0,0 +1,225 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+var interval;
+var player;
+
+
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+
+	player = new GameObject({x:100, y:canvas.height/2-100});
+
+	platform0 = new GameObject();
+		platform0.width = 200;
+		platform0.x = platform0.width/2;
+		platform0.y = canvas.height - platform0.height/2;
+		platform0.color = "#66ff33";
+
+	platform1 = new GameObject();
+		platform1.width = 200;
+		platform1.x = 400;
+		platform1.y = 600;
+		platform1.color = "#FFFFFF";
+
+	platform2 = new GameObject();
+		platform2.width = 200;
+		platform2.x = 800;
+		platform2.y = canvas.height - platform0.height/2;
+		platform2.color = "#FFFFFF";
+
+	platform3 = new GameObject();
+		platform3.width = 200;
+		platform3.x = 1050;
+		platform3.y = 600;
+		platform3.color = "#FFFFFF";
+
+	platform4 = new GameObject();
+		platform4.width = 200;
+		platform4.x = 800;
+		platform4.y = 400;
+		platform4.color = "#FFFFFF";
+		
+	goal = new GameObject({width:24, height:50, x:canvas.width-50, y:100, color:"#00ffff"});
+	
+
+	var fX = .85;
+	var fY = .97;
+	
+	var gravity = 1;
+
+	interval = 1000/60;
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	
+	context.clearRect(0,0,canvas.width, canvas.height);	
+
+	if(w && player.canJump && player.vy ==0)
+	{
+		player.canJump = false;
+		player.vy += player.jumpHeight;
+	}
+
+	if(a)
+	{
+		player.vx += -player.ax * player.force;
+	}
+	if(d)
+	{
+		player.vx += player.ax * player.force;
+	}
+
+	player.vx *= fX;
+	player.vy *= fY;
+	
+	player.vy += gravity;
+	
+	player.x += Math.round(player.vx);
+	player.y += Math.round(player.vy);
+	
+
+	while(platform0.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform0.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform0.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform0.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+
+	while(platform1.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform1.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform1.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform1.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+
+	while(platform2.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform2.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform2.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform2.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+
+	while(platform3.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform3.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform3.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform3.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+
+	while(platform4.hitTestPoint(player.bottom()) && player.vy >=0)
+	{
+		player.y--;
+		player.vy = 0;
+		player.canJump = true;
+	}
+	while(platform4.hitTestPoint(player.left()) && player.vx <=0)
+	{
+		player.x++;
+		player.vx = 0;
+	}
+	while(platform4.hitTestPoint(player.right()) && player.vx >=0)
+	{
+		player.x--;
+		player.vx = 0;
+	}
+	while(platform4.hitTestPoint(player.top()) && player.vy <=0)
+	{
+		player.y++;
+		player.vy = 0;
+	}
+	
+	
+	//---------Objective: Treasure!!!!!!!---------------------------------------------------------------------------------------------------- 
+	//---------Run this program first.
+	//---------Get Creative. Find a new way to get your player from the platform to the pearl. 
+	//---------You can do anything you would like except break the following rules:
+	//---------RULE1: You cannot spawn your player on the pearl!
+	//---------RULE2: You cannot change the innitial locations of platform0 or the goal! 
+		
+	if(player.hitTestObject(goal))
+	{
+		goal.y = 10000;
+		platform1.color = "#474747"
+		platform2.color = "#474747"
+		platform3.color = "#474747"
+		platform4.color = "#474747"
+		context.textAlign = "center";
+		context.drawText("You Win!!!", canvas.width/2, canvas.height/2);
+	}
+	
+	
+	platform0.drawRect();
+	platform1.drawRect();
+	platform2.drawRect();
+	platform3.drawRect();
+	platform4.drawRect();
+
+	//Show hit points
+	player.drawRect();
+	goal.drawCircle();
+}
+
